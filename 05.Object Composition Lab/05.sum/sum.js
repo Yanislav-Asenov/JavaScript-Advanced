c3cbf42afba7149d17a3dab7c3012033422f213b
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-10-24 02:38:06 +0300

    7/8 done TODO

--- 05.Object Composition Lab/05.sum/sum.js
+++ 05.Object Composition Lab/05.sum/sum.js
@@ -1,48 +1,36 @@
-function solve () {
-    let resultObject = (() => {
-        let domObject = {};
-        
-        return {
-            init: (selector1, selector2, resultSelector) => {
-                domObject.firstNum = $(selector1);
-                domObject.secondNum = $(selector2);
-                domObject.resultBox = $(resultSelector);
-            },
-            add: () => {
-                domObject.firstNum = Number($('#num1').val());
-                domObject.secondNum = Number($('#num2').val());
-                let resultValue = domObject.firstNum + domObject.secondNum;
-                domObject.resultBox.val(resultValue);
-            },
-            subtract: () => {
-                domObject.firstNum = Number($('#num1').val());
-                domObject.secondNum = Number($('#num2').val());
-                let resultValue = domObject.firstNum - domObject.secondNum;
-                domObject.resultBox.val(resultValue);
-            }
+function result () {
+    let domObject = {
+        num1: '',
+        num2: '',
+        result: '',
+        init: (selector1, selector2, resultSelector) => {
+                domObject.num1 = $(selector1);
+                domObject.num2 = $(selector2);
+                domObject.result = $(resultSelector);
+        },
+        add: () => {
+            let firstNum = Number(domObject.num1.val());
+            let secondNum = Number(domObject.num2.val());
+            let resultValue = firstNum + secondNum;
+            domObject.result.val(resultValue);
+        },
+        subtract: () => {
+            let firstNum = Number(domObject.num1.val());
+            let secondNum = Number(domObject.num2.val());
+            let resultValue = firstNum - secondNum;
+            domObject.result.val(resultValue);
         }
-    })();
+    };
 
-    return resultObject;
+    return domObject;
 }
 
+// let obj = solve();
 
-// console.log($('#num1'));
-// // let num1 = $('#num1');
-// // let num2 = $('#num2');
-// // let res = $('#result');
-// // num1.val(5);
-// // num2.val(3);
-// // obj.add();
-
-// $(document).ready(() => {
-//     let obj = solve();
-//     obj.init("#num1","#num2",'#result');
-
-//     $('#sumButton').on('click', () => {
-//         obj.add();
-//     });
-//     $('#subtractButton').on('click', () => {
-//         obj.subtract();
-//     });
-// });
+// obj.init("#num1","#num2",'#result');
+// let num1 = $('#num1');
+// let num2 = $('#num2');
+// let res = $('#result');
+// num1.val(5);
+// num2.val(3);
+// obj.add();
