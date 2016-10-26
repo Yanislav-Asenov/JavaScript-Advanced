6741246db7b49b002249df217c2fc6959451f294
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-10-26 21:53:58 +0300

    solved

---
+++ 09.Unit Testing with Mocha Exercises/02.char lookup/char-lookup.js
@@ -0,0 +1,12 @@
+function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect Index";
+    }
+
+    return string.charAt(index);
+}
+
+module.exports = { lookupChar };
