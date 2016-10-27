f149eca52eef35c62c62ab5eba368379caecdfbc
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-10-28 02:58:06 +0300

    100% solved exercises 8/8

--- 06.Advanced Functions, Function Context/07.monkey-patcher.js
+++ 06.Advanced Functions, Function Context/07.monkey-patcher.js
@@ -1,98 +1,52 @@
-let result = (function () {
-     let post = {
-        id: 'Яни',
-        author: '',
-        content: '',
-        upvotes: 0,
-        downvotes: 0,
-    };
+function votingSystem(action) {
+    let that = this;
+    let patcher = (function () {
+        function upvote() {
+            that.upvotes++;
+        }
 
-    function calculateRating (upvotes, downvotes, totalVotes, score) {
-        if (totalVotes >= 10) {
-            if (upvotes / totalVotes > 0.66) {
-                return 'hot';
-            } else if (score >= 0 && (upvotes > 100 || downvotes > 100)) {
-                return 'controversial';
-            } else if (upvotes - downvotes < 0) {
-                return 'unpopular';
+        function downvote() {
+            that.downvotes++;
+        }
+
+        function fakeRecords(num) {
+            return 0.25 * num;
+        }
+
+        function calculateRating() {
+            let totalVotes = that.upvotes + that.downvotes;
+          
+             if (totalVotes >= 10) {
+                if (that.upvotes / totalVotes > 0.66) {
+                    return 'hot';
+                } else if (that.upvotes - that.downvotes >= 0 && (that.upvotes > 100 || that.downvotes > 100)) {
+                    return 'controversial';
+                } else if (that.upvotes - that.downvotes < 0) {
+                    return 'unpopular';
+                }
             }
+          
+          return 'new';
         }
 
-        return 'new';
-    }
+        function score() {
+            let up = that.upvotes;
+            let down = that.downvotes;
 
-    function score () {
-        let totalVotes = post.upvotes + post.downvotes;
-        let upvotes = post.upvotes;
-        let downvotes = post.downvotes;
-        let score = upvotes - downvotes;
-        let rating = calculateRating(upvotes, downvotes, totalVotes, score);
+            let totalScore = that.upvotes - that.downvotes;
+            if (that.upvotes + that.downvotes > 50) {
+                let additionalVotes = fakeRecords(Math.max(that.upvotes, that.downvotes));
+                up = Math.ceil(that.upvotes + additionalVotes);
+                down = Math.ceil(that.downvotes + additionalVotes);
+            }
 
+            let rating = calculateRating();
 
-        if (totalVotes > 50) {
-            let numberToAddToVotes = Math.ceil(Math.max(post.upvotes, post.downvotes) * 0.25);
-            upvotes += numberToAddToVotes;
-            downvotes += numberToAddToVotes;
+            return [up, down, totalScore, rating];
         }
 
-        return [upvotes, downvotes, score, rating];
-    }
+        return { upvote, downvote, score };
+    })();
 
-    function upvote () {
-        post.upvotes++;
-    }
-
-    function downvote () {
-        post.downvotes++;
-    }
-
-    function call (postObject, command) {
-        if (post.id === 'Яни') {
-            post.id = postObject.id;
-            post.author = postObject.author;
-            post.content = postObject.content;
-            post.upvotes = postObject.upvotes;
-            post.downvotes = postObject.downvotes;
-        }
-
-        switch (command) {
-            case 'upvote':
-                upvote();
-                break;
-            case 'downvote':
-                downvote();
-                break;
-            case 'score':
-                return score();
-        }
-    }
-
-    return {
-        call: (inputObj, command) => call(inputObj, command)
-    };
-})();
-
-
-var forumPost = {
-    id: '3',
-    author: 'emil',
-    content: 'wazaaaaa',
-    upvotes: 100,
-    downvotes: 100
-};
-
-
-result.call(forumPost, 'upvote');
-result.call(forumPost, 'downvote');
-
-var answer = result.call(forumPost, 'score');
-console.log(answer);
-
-// for (let i = 0; i < 50; i++) {
-//     result.call(forumPost, 'downvote');
-// }
-
-// answer = result.call(forumPost, 'score');
-
-// console.log(answer);
-// expected = [139, 189, -50, 'unpopular'];
+    return patcher[action]();
+}
