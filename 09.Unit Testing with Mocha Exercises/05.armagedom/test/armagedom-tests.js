b261ac990254d74bbfcd8a0708f38529b6066a7b
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-10-30 15:01:06 +0200

    Solved

--- 09.Unit Testing with Mocha Exercises/05.armagedom/test/armagedom-tests.js
+++ 09.Unit Testing with Mocha Exercises/05.armagedom/test/armagedom-tests.js
@@ -2,27 +2,47 @@ let expect = require('chai').expect;
 let jsdom = require('jsdom-global')();
 let $ = require('jquery');
 
-document.body.innerHTML = `<div id="target test">
-    <div id="target test" class="nested target">
-        <p id="target test">This is some text</p>
-    </div>
-    <div id="target test" class="target">
-        <p>Empty div</p>
-    </div>
-    <div class="inside">
-        <span class="nested">Some more text</span>
-        <span class="target">Some more text</span>
-    </div>
-</div>
-`;
-
 function nuke (selector1, selector2) {
     if (selector1 === selector2) return;
     $(selector1).filter(selector2).remove();
 }
 
 describe('nuke', () => {
-    it('should operates in HTML document', function () {
-        expect($('body').length).to.equal(1, 'The function is not operating in HTML document');
+    let testObject;
+    beforeEach('init the object', function () {
+        document.body.innerHTML =
+            `<div id="target">
+                <div class="nested target">
+                    <p>This is some text</p>
+                </div>
+                <div class="target">
+                    <p>Empty div</p>
+                </div>
+                <div class="inside">
+                    <span class="nested">Some more text</span>
+                    <span class="target">Some more text</span>
+                </div>
+            </div>`;
+    });
+
+    it('should not delete element which do not match the selectors', () => {
+        let selector = $('#target');
+        let prev = selector.html();
+        nuke('.nested', '.inside');
+        expect(selector.html()).to.equal(prev);
+    });
+
+    it('should do nthing on invalid selectors', function () {
+        let expectedOutput = $('#target').html();
+        nuke(99, '#target');
+        let input = $('#target').html();
+        expect(input).to.equal(expectedOutput, 'Function deleted elements on invalid input');
+    });
+   
+    it('should not delete elements if both selectors are the same', function () {
+        let expectedOutput = $('.target').filter('.target').html();
+        nuke('.target', '.target');
+        let input = $('.target').filter('.target').html();
+        expect(input).to.equal(expectedOutput, 'Function deleted elements when both selectors are the same');
     });
 });
